c9fa024af4a5b45b03d50afcd5844c324d66af60
Michael Shihjay Chen <shihjay2@gmail.com>
2022-11-30 20:05:33 -0800

    Fix start function

--- index.mjs
+++ index.mjs
@@ -83,114 +83,115 @@ app.get('/start', async(req, res) => {
   var opts = JSON.parse(JSON.stringify(settings.couchdb_auth))
   objectPath.set(opts, 'skip_setup', true)
   const check = new PouchDB((settings.couchdb_uri + '/users'), opts)
-  try {
-    await check.info()
-    res.redirect(urlFix(req.protocol + '://' + req.hostname + '/') + 'app/login')
-  } catch (e) {
-    // installation needed
-    await couchdbInstall()
-    var b = false
-    for (let a = 0; a < 20; a++) {
-      if (!b) {
-        b = await isReachable(settings.couchdb_uri)
+  var info = await check.info()
+  if (objectPath.has(info, 'error')) {
+    if (info.error == 'not_found') {
+      await couchdbInstall()
+      var b = false
+      for (let a = 0; a < 20; a++) {
+        if (!b) {
+          b = await isReachable(settings.couchdb_uri)
+        }
       }
-    }
-    if (b) {
-      await couchdbDatabase()
-      const users = new PouchDB((settings.couchdb_uri + '/_users'), settings.couchdb_auth)
-      await users.info()
-      const db_users = new PouchDB((settings.couchdb_uri + '/users'), settings.couchdb_auth)
-      var result = await db_users.find({selector: {_id: {$regex: "^nosh_*"}}})
-      if (result.docs.length === 0) {
-        const id = 'nosh_' + uuidv4()
-        var user = {
-          display: process.env.NOSH_DISPLAY,
-          id: id,
-          _id: id,
-          email: process.env.NOSH_EMAIL,
-          role: process.env.NOSH_ROLE,
-          did: process.env.NOSH_DID
-        }
-        const id1 = 'nosh_' + uuidv4()
-        if (user.role === 'patient') {
-          var patient = {
-            "_id": id1,
-            "resourceType": "Patient",
-            "id": id1,
-            "name": [
-              {
-                "family": process.env.NOSH_LASTNAME,
-                "given": [
-                  process.env.NOSH_FIRSTNAME
-                ],
-                "use": "official",
-              }
-            ],
-            "text": {
-              "status": "generated",
-              "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\">" + process.env.NOSH_FIRSTNAME + ' ' + process.env.NOSH_LASTNAME + "</div>"
-            },
-            "birthDate": process.env.NOSH_BIRTHDAY,
-            "gender": process.env.NOSH_GENDER,
-            "extension": [
-              {
-                "url": "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race"
-              },
-              {
-                "url": "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity"
-              },
-              {
-                "url": "http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex",
-                "valueCode": process.env.NOSH_BIRTHGENDER
-              }
-            ]
+      if (b) {
+        await couchdbDatabase()
+        const users = new PouchDB((settings.couchdb_uri + '/_users'), settings.couchdb_auth)
+        await users.info()
+        const db_users = new PouchDB((settings.couchdb_uri + '/users'), settings.couchdb_auth)
+        var result = await db_users.find({selector: {_id: {$regex: "^nosh_*"}}})
+        if (result.docs.length === 0) {
+          const id = 'nosh_' + uuidv4()
+          var user = {
+            display: process.env.NOSH_DISPLAY,
+            id: id,
+            _id: id,
+            email: process.env.NOSH_EMAIL,
+            role: process.env.NOSH_ROLE,
+            did: process.env.NOSH_DID
           }
-          const db1 = new PouchDB('patients')
-          await db1.setPassword(process.env.COUCHDB_ENCRYPT_PIN, {name: settings.couchdb_uri + '/patients', opts: settings.couchdb_auth})
-          await db1.put(patient)
-          objectPath.set(user, 'reference', 'Patient/' + id1)
-        }
-        if (user.role === 'provider') {
-          var practitioner = {
-            "_id": id1,
-            "resourceType": "Practitioner",
-            "id": id1,
-            "name": [
-              {
-                "family": process.env.NOSH_LASTNAME,
-                "use": "official",
-                "given": [
-                  process.env.NOSH_FIRSTNAME
-                ],
-                "suffix": [
-                  process.env.NOSH_SUFFIX
-                ]
-              }
-            ],
-            "text": {
-              "status": "generated",
-              "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\">" + process.env.NOSH_FIRSTNAME + ' ' + process.env.NOSH_LASTNAME + ', ' + process.env.NOSH_SUFFIX + "</div>"
+          const id1 = 'nosh_' + uuidv4()
+          if (user.role === 'patient') {
+            var patient = {
+              "_id": id1,
+              "resourceType": "Patient",
+              "id": id1,
+              "name": [
+                {
+                  "family": process.env.NOSH_LASTNAME,
+                  "given": [
+                    process.env.NOSH_FIRSTNAME
+                  ],
+                  "use": "official",
+                }
+              ],
+              "text": {
+                "status": "generated",
+                "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\">" + process.env.NOSH_FIRSTNAME + ' ' + process.env.NOSH_LASTNAME + "</div>"
+              },
+              "birthDate": process.env.NOSH_BIRTHDAY,
+              "gender": process.env.NOSH_GENDER,
+              "extension": [
+                {
+                  "url": "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race"
+                },
+                {
+                  "url": "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity"
+                },
+                {
+                  "url": "http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex",
+                  "valueCode": process.env.NOSH_BIRTHGENDER
+                }
+              ]
             }
+            const db1 = new PouchDB('patients')
+            await db1.setPassword(process.env.COUCHDB_ENCRYPT_PIN, {name: settings.couchdb_uri + '/patients', opts: settings.couchdb_auth})
+            await db1.put(patient)
+            objectPath.set(user, 'reference', 'Patient/' + id1)
           }
-          const db2 = new PouchDB('practitioners')
-          await db2.setPassword(process.env.COUCHDB_ENCRYPT_PIN, {name: settings.couchdb_uri + '/practitioners', opts: settings.couchdb_auth})
-          await db2.put(practitioner)
-          objectPath.set(user, 'reference', 'Practitioner/' + id1)
-          objectPath.set(user, 'templates', JSON.parse(fs.readFileSync('./assets/templates.json')))
-        }
-        await db_users.put(user)
-        if (user.role === 'patient') {
-          res.redirect(urlFix(req.protocol + '://' + req.hostname + '/') + 'app/chart/' + id1)
-        }
-        if (user.role === 'provider') {
-          res.redirect(urlFix(req.protocol + '://' + req.hostname + '/') + 'app/dashboard')
+          if (user.role === 'provider') {
+            var practitioner = {
+              "_id": id1,
+              "resourceType": "Practitioner",
+              "id": id1,
+              "name": [
+                {
+                  "family": process.env.NOSH_LASTNAME,
+                  "use": "official",
+                  "given": [
+                    process.env.NOSH_FIRSTNAME
+                  ],
+                  "suffix": [
+                    process.env.NOSH_SUFFIX
+                  ]
+                }
+              ],
+              "text": {
+                "status": "generated",
+                "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\">" + process.env.NOSH_FIRSTNAME + ' ' + process.env.NOSH_LASTNAME + ', ' + process.env.NOSH_SUFFIX + "</div>"
+              }
+            }
+            const db2 = new PouchDB('practitioners')
+            await db2.setPassword(process.env.COUCHDB_ENCRYPT_PIN, {name: settings.couchdb_uri + '/practitioners', opts: settings.couchdb_auth})
+            await db2.put(practitioner)
+            objectPath.set(user, 'reference', 'Practitioner/' + id1)
+            objectPath.set(user, 'templates', JSON.parse(fs.readFileSync('./assets/templates.json')))
+          }
+          await db_users.put(user)
+          if (user.role === 'patient') {
+            res.redirect(urlFix(req.protocol + '://' + req.hostname + '/') + 'app/chart/' + id1)
+          }
+          if (user.role === 'provider') {
+            res.redirect(urlFix(req.protocol + '://' + req.hostname + '/') + 'app/dashboard')
+          }
+        } else {
+          res.status(200).send('NOSH is already installed')
         }
       } else {
-        res.status(200).send('NOSH is already installed')
+        res.status(200).send('CouchDB is not restarting for some reason; try again')
       }
-    } else {
-      res.status(200).send('CouchDB is not restarting for some reason; try again')
     }
+  } else {
+    res.redirect(urlFix(req.protocol + '://' + req.hostname + '/') + 'app/login')
   }
 })
 
