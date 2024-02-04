fff42e6057f5a2e6ffda0556e2f184226450173b
Michael Shihjay Chen <shihjay2@gmail.com>
2024-02-03 22:18:58 -0800

    fix rules change

--- nosh3-client/src/main.js
+++ nosh3-client/src/main.js
@@ -6,7 +6,7 @@ import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 import { router } from './helpers'
 import { defineRule, configure } from 'vee-validate'
-import AllRules from '@vee-validate/rules'
+import * as rules from '@vee-validate/rules'
 
 const app = createApp(App)
 app.use(Quasar, quasarUserOptions)
@@ -14,8 +14,8 @@ app.use(createPinia())
 app.use(router)
 app.mount('#app')
 
-Object.keys(AllRules).forEach(rule => {
-  defineRule(rule, AllRules[rule])
+Object.keys(rules).forEach(rule => {
+  defineRule(rule, rules[rule])
 })
 
 configure({
