d0f046e0205c3004e77a74d20e345f2e640ccb4c
Michael Shihjay Chen <shihjay2@gmail.com>
2024-05-01 10:44:57 -0700

    fix ui

--- nosh3-client/src/components/QDrawerTemplate.vue
+++ nosh3-client/src/components/QDrawerTemplate.vue
@@ -80,7 +80,7 @@
         </q-chip>
       </q-item>
       <div v-for="item in state.ui" :key="item.resource">
-        <q-item clickable @click="open(item.type, item.resource, item.category, '')" v-ripple>
+        <q-item dense clickable @click="open(item.type, item.resource, item.category, '')" v-ripple>
           <q-item-section avatar>
             <q-icon color="primary" :name="item.icon" />
           </q-item-section>
