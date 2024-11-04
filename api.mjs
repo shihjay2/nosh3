216655f6234d47611d530057f355d998982fabac
Michael Shihjay Chen <shihjay2@gmail.com>
2024-11-03 22:27:14 -0800

    cleanup

--- api.mjs
+++ api.mjs
@@ -47,25 +47,27 @@ async function getTimeline(req, res) {
       }
       if (row.resource === 'document_references') {
         const doc = row.doc
-        if (objectPath.has(doc, 'content')) {
-          for (const c in objectPath.get(doc, 'content')) {
-            const binary_id = objectPath.get(doc, 'content.' + c + '.attachment.url').substring(objectPath.get(doc, 'content.' + c + '.attachment.url').indexOf('/') + 1)
-            const binary_doc = await db_binary.get(binary_id)
-            const data = atob(objectPath.get(binary_doc, 'data'))
-            if (objectPath.get(doc, 'content.' + c + '.attachment.contentType').includes('text/plain')) {
-              if (isMarkdown(data)) {
-                const md_arr = markdownParse(data)
-                for (const md_arr_row of md_arr) {
-                  mdjs.push(md_arr_row)
+        if (doc.description !== 'AI Chat') {
+          if (objectPath.has(doc, 'content')) {
+            for (const c in objectPath.get(doc, 'content')) {
+              const binary_id = objectPath.get(doc, 'content.' + c + '.attachment.url').substring(objectPath.get(doc, 'content.' + c + '.attachment.url').indexOf('/') + 1)
+              const binary_doc = await db_binary.get(binary_id)
+              const data = atob(objectPath.get(binary_doc, 'data'))
+              if (objectPath.get(doc, 'content.' + c + '.attachment.contentType').includes('text/plain')) {
+                if (isMarkdown(data)) {
+                  const md_arr = markdownParse(data)
+                  for (const md_arr_row of md_arr) {
+                    mdjs.push(md_arr_row)
+                  }
                 }
               }
-            }
-            if (objectPath.get(doc, 'content.' + c + '.attachment.contentType').includes('text/html')) {
-              const turndownService = new TurndownService()
-              const md = turndownService.turndown(data)
-              const md_arr1 = markdownParse(md)
-              for (const md_arr_row1 of md_arr1) {
-                mdjs.push(md_arr_row1)
+              if (objectPath.get(doc, 'content.' + c + '.attachment.contentType').includes('text/html')) {
+                const turndownService = new TurndownService()
+                const md = turndownService.turndown(data)
+                const md_arr1 = markdownParse(md)
+                for (const md_arr_row1 of md_arr1) {
+                  mdjs.push(md_arr_row1)
+                }
               }
             }
           }
